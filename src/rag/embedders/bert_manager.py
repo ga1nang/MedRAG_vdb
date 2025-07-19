21039998d8ef02f60838f920d77758c93b72c2a1
ga1nang <nnthanhmx@gmail.com>
2025-07-19 14:35:19 +0700

    feat: add new function to embed the symtoms

--- src/rag/embedders/bert_manager.py
+++ src/rag/embedders/bert_manager.py
@@ -1,4 +1,7 @@
 import torch
+import os
+import tqdm
+import numpy as np
 from transformers import AutoTokenizer, AutoModel
 
 class BERTManger:
@@ -14,12 +17,26 @@ class BERTManger:
     def generate_embedding(self, query):
         # Tokenize sentences
         inputs = self.tokenizer(query, padding=True, truncation=True, return_tensors='pt')
-
         # Compute token embeddings
         with torch.no_grad():
             output = self.model(**inputs)
-
         # Perform pooling. In this case, mean pooling.
         embeddings = self.meanpooling(output, inputs['attention_mask'])
-
         return embeddings
+    
+    def get_symptom_embeddings(self, symptom_nodes, save_path):
+        """Generate or load embeddings for symptom nodes"""
+        embeddings_path = os.path.join(save_path, 'KG_embeddings.npy')
+        if os.path.exists(embeddings_path):
+            print("load existing embeddings...")
+            return np.load(embeddings_path)
+        else:
+            print("generate new embeddings...")
+            symptom_embeddings = []
+            # Generate embedding for each symptom
+            for symptom in tqdm(symptom_nodes):
+                symtom_embedding = self.generate_embedding(symptom)
+                symptom_embeddings.append(symtom_embedding)
+            # Save the embeddings for other use
+            np.save(embeddings_path, symptom_embeddings)
+            return np.array(symptom_embeddings)
